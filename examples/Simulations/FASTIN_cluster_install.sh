d0b784448d5f119447ff01d8772f21d26a30528a
Philipp Neubauer <philipp@haast.dragonfly.co.nz>
2014-03-31 13:34:36 +1300

    bitta cleanup

--- examples/Simulations/FASTIN_cluster_install.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/Philipp-Neubauer/FASTIN-R.git
-
-cd FASTIN-R
-
-echo 'install.packages(c("vegan", "fgui", "rjags", "robCompositions", "compositions", "denstrip"),repos="http://cran.stat.auckland.ac.nz/",dependencies=T);install.packages("FASTIN-R",repos=NULL)' > inst.pkg.R
-
-R CMD BATCH inst.pkg.R
--- examples/Simulations/install_all_cluster.R
+++
@@ -1,9 +0,0 @@
-for (i in 1:length(cl)){
-    
-    system(paste('scp FASTIN_cluster_install.sh philipp@',cl[i],':/home/philipp/',sep=''))
-    
-    system(paste('ssh',cl[i],'chmod 700 FASTIN_cluster_install.sh'))
-
-    system(paste('ssh',cl[i],'./FASTIN_cluster_install.sh'))
-
-}
