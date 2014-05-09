c59cb61610bf7c65f82a05233119537ccf5a0f62
Philipp Neubauer <neubauer.phil@gmail.com>
2014-05-09 13:31:47 +1200

    add sensitivity subset

---
+++ fastinR/R/sensitivity.R
@@ -0,0 +1,31 @@
+#' Simulated Fatty Acid and Stable Isotope example
+#' 
+#' Simulated datasets to illustrate model sensitivity
+#' 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name uneven_good
+
+NULL
+
+#' Simulated Fatty Acid and Stable Isotope example
+#' 
+#' Simulated datasets to illustrate model sensitivity
+#' 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name even_good
+
+NULL
+#' Simulated Fatty Acid and Stable Isotope example
+#' 
+#' Simulated datasets to illustrate model sensitivity
+#' 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name even_bad
+
+NULL
